0f4c9b77b3f1f1d7260a226aa0a8d2f1becd0455
HRITIK UPADHYAY <hritikupadhyay134@gamil.com>
2023-10-18 13:06:30 +0530

    First commit for Todo Assignement

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function implementAddTodoList(e) {
+    e.preventDefault();
+    setTodoList([...todoList, todo]);
+    setTodo("");
+  }
+
+  function deleteTodo(idx) {
+    const newTodoList = todoList.filter((item, index) => index !== idx);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p> To Do List </p>
+        <form onSubmit={implementAddTodoList}>
+          <input type="text" placeholder="Enter Task" onChange={(e) => setTodo(e.target.value)} value={todo}/>
+          <button type="submit"> To do </button>
+        </form> 
+      
+      {
+        todoList.map((item, index) => (
+          <div>
+            <ul>
+              <li> 
+                {item} 
+                <button onClick={() => deleteTodo(index)}> Delete </button>
+              </li>
+            </ul>  
+          </div>
+        ))
+      }
+
     </div>
   )
 }
 
-export default App
+export default App;
